886dd4e14fe80ff96fcec46b975d19c785dcdf84
Xuguangxing <Xugx29@163.com>
2021-05-05 14:54:45 +0800

    'fix'

--- service/MongoService.js
+++ service/MongoService.js
@@ -1,11 +1,10 @@
-(function () {
-    require('../schema/mongoSchema')
-})()
 const User = require('../model/Mongo')
 class MongoService {
     static async get () {
-        return await User.find().where({
-            role: {$ne: null}
+        return await User.create({
+            name: 'abc',
+            role: 'test',
+            id: 10
         })
         // return 123;
     }
